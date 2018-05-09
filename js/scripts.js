51a250a70b0f897122d1bc8530bdddacfe019cd7
Tejas-Ozarkar <tejozarkar@gmail.com>
2018-05-09 20:41:59 +0530

    Fix loader 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-$(document).ready(function() {
-    $('#loader').css("visibility", "hidden");
-    $('#load').css("visibility", "hidden");
-    $('html, body').css("overflow-y", "auto");
-})
+document.onreadystatechange = function() {
+    var state = document.readyState
+    if (state == 'complete') {
+        $('#loader').css("visibility", "hidden");
+        $('#load').css("visibility", "hidden");
+        $('html, body').css("overflow-y", "auto");
+    }
+}

29783b990000cc9d6204a7607e536ced2369a32f
Tejas-Ozarkar <tejozarkar@gmail.com>
2018-05-09 20:30:01 +0530

    Fix Horizontal Scrolling

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#loader').css("visibility", "hidden");
     $('#load').css("visibility", "hidden");
-    $('html, body').css("overflow", "auto");
+    $('html, body').css("overflow-y", "auto");
 })
